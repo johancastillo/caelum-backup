bcd80068fda62e79889969cbddcc19fdffa22520
Johan Castillo <jcjohan2707@gmail.com>
2021-08-25 11:09:14 -0400

    Backup first

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//
+// This file is meant to regroup your javascript code. You can either copy/past
+// any code that should be executed on each page loading or write your own
+// taking advantage of the Odoo framework to create new behaviors or modify
+// existing ones. For example, doing this will greet any visitor with a 'Hello,
+// world !' message in a popup:
+//
+/*
+odoo.define('website.user_custom_code', function (require) {
+'use strict';
+
+var Dialog = require('web.Dialog');
+var publicWidget = require('web.public.widget');
+
+publicWidget.registry.HelloWorldPopup = publicWidget.Widget.extend({
+    selector: '#wrapwrap',
+
+    start: function () {
+        Dialog.alert(this, "Hello, world!");
+        return this._super.apply(this, arguments);
+    },
+})
+});
+*/
+
+
+let screenSize = window.screen.width
+
+if(screenSize <= 1000){
+    let imgMobile = document.getElementById("img-mobile")
+    imgMobile.style.display = "block"
+    
+    let bannerPrimary = document.getElementById("banner-primary")
+    bannerPrimary.style.display = "none"
+}else{
+    
+}
